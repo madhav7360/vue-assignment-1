3909469daf19b0644ac583a715fe364bc56e6c05
Madhav7360 <madhavmanocha7@gmail.com>
2021-08-24 03:28:17 +0530

    Add event detail page
    * created new view file EventDetail to diaply event deails when card is clicked on
    * added getEvent function to fetch deails of selected event through
      event id

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import About from "../views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
-    component: EventList,
+    path: '/',
+    name: 'EventList',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    component: About
+    path: '/event/123',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
   },
-];
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
