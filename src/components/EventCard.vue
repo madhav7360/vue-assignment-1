9f6a58fdbf734ab20069347a69437159b5740185
Madhav7360 <madhavmanocha7@gmail.com>
2021-08-23 19:28:47 +0530

    Refactor Home view
    * moved dummy data from EventCard componet to Home view
    * looping over dummy data to display all data in multiple cards
    * added style to enclose event cards in flexbox

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,33 +4,18 @@
    <span>@ 
      {{event.time}} on {{event.date}}
    </span>
-   <h2>
+   <h3>
      {{event.title}}
-   </h2>
+   </h3>
   </div>
 </template>
 
 <script>
 export default {
   name: "EventCard",
-  //props: {
-  //  msg: String,
-  //},
-  data() {
-    return{
-      event: {
-          id: 5928101,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-       }
-    }
-  }
+  props: {
+    event: Object
+  },
 };
 </script>
 
